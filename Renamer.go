e5bb1c1252018f81da906205e1c0a5c57a87dba2
Gianluca Fiore <forod.g@gmail.com>
2015-01-01 09:16:09 +0100

    update copyright year

--- Renamer.go
+++ Renamer.go
@@ -3,7 +3,7 @@ renamer.go
 
 Renaming files script on various patterns
 
-Author: Gianluca Fiore <forod.g@gmail.com> © 2013-2014
+Author: Gianluca Fiore <forod.g@gmail.com> © 2013-2015
 */
 
 package main
